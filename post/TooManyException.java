e631222004dc9cf2d5793c0f696041bbd1aeb2f4
Gustavo Lima <121981603+GustavoH-Lima@users.noreply.github.com>
2023-06-15 19:10:46 -0300

    Criei a exceção

--- post/TooManyException.java
+++ post/TooManyException.java
@@ -2,6 +2,9 @@ package trab_poo.post;
 
 public class TooManyException extends Throwable
 {
-    
-    
+	@Override
+    public String toString()
+	{
+		return "Há muitas fotos na postagem";	
+	}
 }
