99ebf12133bd51f122e3271a5b936b8fbc719f2a
Gustavo Lima <121981603+GustavoH-Lima@users.noreply.github.com>
2023-06-22 11:29:23 -0300

    Update Postavel.java

--- post/Postavel.java
+++ post/Postavel.java
@@ -3,6 +3,8 @@ package trab_poo.post;
 public abstract class Postavel
 {
 	public abstract boolean posta() throws TooFewException,TooManyException;;
-	public abstract boolean comenta();
+	public abstract boolean comenta(String texto);
 	public abstract void infos();
+	public abstract int getNum_comentarios();
+	public abstract String getUltimoComent();
 }
